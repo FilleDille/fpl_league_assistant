8af4057770883095f4b4fe3bff6d18d4f3cd49c2
filipengstrom <filipengstrom91@gmail.com>
2023-07-08 08:05:07 +0200

    fixed a poc

--- main.py
+++ main.py
@@ -4,6 +4,39 @@ import requests as rq
 import sys
 import json
 import smtplib
+import time
+
+
+class Participant:
+    def __init__(self, player_id):
+        self.player_url = f'https://fantasy.premierleague.com/api/entry/{player_id}/'
+        response_raw = rq.get(self.player_url)
+        self.response = response_raw.json()
+        self.id = player_id
+
+        try:
+            self.team_name = self.response['name']
+
+        except Exception as e:
+            self.team_name = e
+
+        try:
+            self.first_name = self.response['player_first_name']
+
+        except Exception as e:
+            self.first_name = e
+
+        try:
+            self.last_name = self.response['player_last_name']
+
+        except Exception as e:
+            self.last_name = e
+
+        try:
+            self.country = self.response['player_region_name']
+
+        except Exception as e:
+            self.country = e
 
 
 class FPL:
@@ -12,11 +45,11 @@ class FPL:
 
         self.league_id = league_id
         self.league_name = ''
-        self.missing_participants = []
-        self.new_participants = []
-        self.response = None
-        self.response_results = None
-        self.participants = 0
+        self.missing_participants_id = []
+        self.new_participants_id = []
+        self.missing_participants_obj = []
+        self.new_participants_obj = []
+        self.response_results = []
         self.league_url = f'https://fantasy.premierleague.com/api/leagues-classic/{self.league_id}/standings/'
 
         if sys.platform == "darwin":
@@ -38,15 +71,31 @@ class FPL:
                 self.current_participants_json = []
 
     def fetch(self):
-        response_raw = rq.get(self.league_url)
-        self.response = response_raw.json()
-        self.response_results = self.response['new_entries']['results']
-        self.participants = len(self.response)
-        self.league_name = self.response['league']['name']
+        has_more = True
+        i = 1
+
+        while has_more:
+            if i == 800:
+                has_more = False
+
+            response_raw = rq.get(self.league_url + f'?page_new_entries={i}&page_standings=1&phase=1')
+            response = response_raw.json()
+
+            if self.league_name == '':
+                self.league_name = response['league']['name']
+
+            if len(response['new_entries']['results']) == 0:
+                has_more = False
+            else:
+                self.response_results = self.response_results + response['new_entries']['results']
+
+            time.sleep(0.1)
+            i += 1
+
 
     def compare(self):
-        self.missing_participants = []
-        self.new_participants = []
+        self.missing_participants_id = []
+        self.new_participants_id = []
 
         response_entries = [entry['entry'] for entry in self.response_results]
         missing_entries = [entry for entry in self.current_participants_json if entry['entry'] not in response_entries]
@@ -54,23 +103,41 @@ class FPL:
                        if not any(d['entry'] == entry['entry'] for d in self.current_participants_json)]
 
         for entry in missing_entries:
-            self.missing_participants.append(entry['entry'])
+            self.missing_participants_id.append(entry['entry'])
 
         for entry in new_entries:
-            self.new_participants.append(entry['entry'])
+            self.new_participants_id.append(entry['entry'])
 
-        if len(self.missing_participants) > 0 or len(self.new_participants) > 0:
+        if len(self.missing_participants_id) > 0 or len(self.new_participants_id) > 0:
+            self.missing_participants_obj = [Participant(entry) for entry in self.missing_participants_id]
+            self.new_participants_obj = [Participant(entry) for entry in self.new_participants_id]
+
             with open(self.participant_path, 'w') as file:
+                self.send_email()
                 json.dump(self.response_results, file)
 
     def send_email(self):
+        missing_list = [f'{player.id}, ' \
+                        f'{player.team_name}, ' \
+                        f'{player.first_name},' \
+                        f'{player.last_name}, ' \
+                        f'{player.country}'
+                        for player in self.missing_participants_obj]
+
+        new_list = [f'{player.id}, ' \
+                    f'{player.team_name}, ' \
+                    f'{player.first_name}, ' \
+                    f'{player.last_name}, ' \
+                    f'{player.country}'
+                    for player in self.new_participants_obj]
+
         message = 'Subject: Change in league {}\n\nThe following {} ' \
                   'has left the league:{}\nThe following {} has joined ' \
                   'the league:{}'.format(self.league_name,
-                                         len(self.missing_participants),
-                                         self.missing_participants,
-                                         len(self.new_participants),
-                                         self.new_participants
+                                         len(self.missing_participants_id),
+                                         missing_list,
+                                         len(self.new_participants_id),
+                                         new_list
                                          )
 
         try:
@@ -80,12 +147,8 @@ class FPL:
             s.sendmail(self.email, self.email, message)
             s.quit()
 
-            return True
-
         except Exception as e:
             print(e)
-
-            return False
 
 
 if __name__ == '__main__':
